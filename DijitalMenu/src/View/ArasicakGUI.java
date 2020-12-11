30193adb13b1a22a5a621559d9bedb4434e05c3e
xwork1 <74452826+xwork1@users.noreply.github.com>
2020-12-11 14:21:06 +0300

    xwork1

---
+++ DijitalMenu/src/View/ArasicakGUI.java
@@ -0,0 +1,99 @@
+package View;
+import Help.DBConnection;
+import Main.Menu;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JToggleButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ArasicakGUI extends JFrame {
+
+    private JPanel xw;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ArasicakGUI frame = new ArasicakGUI();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public ArasicakGUI() {
+        setBackground(Color.LIGHT_GRAY);
+        setTitle("ARASICAKLAR");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 720, 420);
+        xw = new JPanel();
+        xw.setBackground(Color.WHITE);
+        xw.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(xw);
+        xw.setLayout(null);
+
+        DBConnection db = new DBConnection();
+        Connection c = db.connDb();
+
+        try {
+            Statement st = c.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM yemekler where kategori_id = '" + 5 + "'");
+            int i = 0;
+            JToggleButton[] btnSicaklarlist = new JToggleButton[50];
+            int h1 = 1;
+            int h,w=-190;
+            while (rs.next()) {
+            	btnSicaklarlist[i] = new JToggleButton(rs.getString("yemekAdi")+"("+rs.getInt("fiyati")+")");
+            	
+                if (i % 3 == 0) {
+                    System.out.println("ok");
+                    h1 = 1;
+                    w+=200;
+                }
+                
+               h = h1 * 10;
+                
+               btnSicaklarlist[i].setBounds(w, h, 190, 88);
+                xw.add(btnSicaklarlist[i]);
+                i++;
+                h1+=10;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ArasicakGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        JButton btnBackButton = new JButton("Geri");
+        btnBackButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                new Menu().setVisible(true);
+                hide();
+            }
+        });
+        btnBackButton.setBounds(611, 10, 85, 21);
+        xw.add(btnBackButton);
+    }
+}
---
+++ DijitalMenu/src/View/BalikGUI.java
@@ -0,0 +1,99 @@
+package View;
+import Help.DBConnection;
+import Main.Menu;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JToggleButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BalikGUI extends JFrame {
+
+    private JPanel xw;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    BalikGUI frame = new BalikGUI();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public BalikGUI() {
+        setBackground(Color.LIGHT_GRAY);
+        setTitle("DENIZ URUNLERI");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 720, 420);
+        xw = new JPanel();
+        xw.setBackground(Color.WHITE);
+        xw.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(xw);
+        xw.setLayout(null);
+
+        DBConnection db = new DBConnection();
+        Connection c = db.connDb();
+
+        try {
+            Statement st = c.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM yemekler where kategori_id = '" + 2 + "'");
+            int i = 0;
+            JToggleButton[] btnBaliklist = new JToggleButton[50];
+            int h1 = 1;
+            int h,w=-190;
+            while (rs.next()) {
+            	btnBaliklist[i] = new JToggleButton(rs.getString("yemekAdi")+"("+rs.getInt("fiyati")+")");
+               
+                if (i % 3 == 0) {
+                    System.out.println("ok");
+                    h1 = 1;
+                    w+=200;
+                }
+                
+               h = h1 * 10;
+                
+               btnBaliklist[i].setBounds(w, h, 190, 88);
+                xw.add(btnBaliklist[i]);
+                i++;
+                h1+=10;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BalikGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        JButton btnBackButton = new JButton("Geri");
+        btnBackButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                new Menu().setVisible(true);
+                hide();
+            }
+        });
+        btnBackButton.setBounds(611, 10, 85, 21);
+        xw.add(btnBackButton);
+    }
+}
